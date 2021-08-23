e02920a575b99cc246f1ed0d57a3a201a768550d
Faraz Shaikh <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-08-23 23:05:58 +0530

    added uc2_rolling_dice

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -3,7 +3,9 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 		int starting_position=0;
+		int dieNumber=(int)(Math.floor(Math.random()*10)%6)+1;
 		System.out.println("Initial position of the player is: "+starting_position);
+		System.out.println("The outcome of rolled dice:"+dieNumber);
 
 	}
 
